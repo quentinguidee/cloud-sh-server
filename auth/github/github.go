c62336c376aafe3f8abc17aff8f8322135987481
Quentin Guidée <quentin.guidee@gmail.com>
2022-06-23 17:59:14 +0200

    Add GitHub Authorize route (wip)
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- auth/github/github.go
+++ auth/github/github.go
@@ -1,11 +1,12 @@
 package github
 
 import (
-	"net/http"
-	"os"
-
+	authContext "context"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/oauth2"
+	"net/http"
+	"os"
 )
 
 func getConfig() oauth2.Config {
@@ -39,8 +40,81 @@ func login(context *gin.Context) {
 	})
 }
 
+type AuthorizeParams struct {
+	Code  string
+	State string
+}
+
+type User struct {
+	Email string
+	Name  string
+}
+
 func authorize(context *gin.Context) {
+	// TODO: Split this function
+
+	var config = getConfig()
+	var params AuthorizeParams
+
+	// Decode JSON
+	err := context.BindJSON(&params)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Body can't be decoded into an AuthorizeParams object.",
+		})
+		return
+	}
+
+	// Ensure the AUTH_STATE is correct.
+	if params.State != os.Getenv("AUTH_STATE") {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "The states don't match.",
+		})
+		return
+	}
+
+	// Generate the Token
+	ctx := authContext.Background()
+	token, err := config.Exchange(ctx, params.Code)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "The server failed to generate your token.",
+		})
+		return
+	}
+
+	// Get user info
+	req, err := http.NewRequest("GET", "https://api.github.com/user", nil)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to fetch GitHub user account.",
+		})
+		return
+	}
+
+	// Get client info
+	client := config.Client(ctx, token)
+	res, err := client.Do(req)
+	if err != nil {
+		context.JSON(res.StatusCode, gin.H{
+			"message": "Failed to fetch GitHub user account.",
+		})
+		return
+	}
+
+	// Decode client info
+	var user User
+	err = json.NewDecoder(res.Body).Decode(&user)
+	if err != nil {
+		context.JSON(res.StatusCode, gin.H{
+			"message": "Failed to decode GitHub user account.",
+		})
+		return
+	}
+
+	// OK
 	context.JSON(http.StatusOK, gin.H{
-		"message": "Server is Running.",
+		"token": token,
+		"user":  user,
 	})
 }
