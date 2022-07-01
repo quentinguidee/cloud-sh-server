28beb4e66690cfc41e8f6ced6d02f41b8993ce74
Hector <54723548+Syycorax@users.noreply.github.com>
2022-07-01 19:50:37 +0200

    Improve error messages for bucket access (#10)
    * Added error message
    
    * fixed formatting

--- commands/storage/create_bucket_access.go
+++ commands/storage/create_bucket_access.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"errors"
 	"net/http"
 	. "self-hosted-cloud/server/commands"
 	. "self-hosted-cloud/server/database"
@@ -35,6 +36,7 @@ func (c CreateBucketAccess) Run() ICommandError {
 	).Scan(&c.BucketAccess.Id)
 
 	if err != nil {
+		err = errors.New("error while creating bucket access")
 		return NewError(http.StatusInternalServerError, err)
 	}
 	return nil
@@ -45,6 +47,7 @@ func (c CreateBucketAccess) Revert() ICommandError {
 
 	_, err := c.Database.Instance.Exec(request, c.BucketAccess.Id)
 	if err != nil {
+		err = errors.New("error while deleting bucket access")
 		return NewError(http.StatusInternalServerError, err)
 	}
 	return nil
