314f477baf57611689e4a3bbe9c8a81cd995ecd5
Syycorax <54723548+Syycorax@users.noreply.github.com>
2022-07-01 19:55:09 +0200

    Improved create bucket node association error

--- commands/storage/create_bucket_node_association.go
+++ commands/storage/create_bucket_node_association.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"errors"
 	"net/http"
 	. "self-hosted-cloud/server/commands"
 	. "self-hosted-cloud/server/database"
@@ -21,6 +22,7 @@ func (c CreateBucketNodeAssociationCommand) Run() ICommandError {
 
 	_, err := c.Database.Instance.Exec(request, c.FromNode.Id, c.ToNode.Id)
 	if err != nil {
+		err = errors.New("error while creating bucket node association")
 		return NewError(http.StatusInternalServerError, err)
 	}
 	return nil
@@ -35,6 +37,7 @@ func (c CreateBucketNodeAssociationCommand) Revert() ICommandError {
 
 	_, err := c.Database.Instance.Exec(request, c.FromNode.Id, c.ToNode.Id)
 	if err != nil {
+		err = errors.New("error while deleting bucket node association")
 		return NewError(http.StatusInternalServerError, err)
 	}
 	return nil
