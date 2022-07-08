218982df34e409c7d4d055dd5ae547cdb981cead
Syycorax <54723548+Syycorax@users.noreply.github.com>
2022-07-08 22:47:14 +0200

    Added extensions for some more icons

--- services/storage/nodetypes.go
+++ services/storage/nodetypes.go
@@ -10,13 +10,54 @@ func DetectFileType(nodeName string) string {
 	extension := path.Ext(nodeName)
 
 	switch extension {
+	case ".babelrc":
+		return "babel"
 	case ".c":
 		return "c"
 	case ".cpp", ".cxx":
 		return "cpp"
+	case ".css":
+		return "css"
+	case ".git", ".gitignore", ".gitattributes", ".gitmodules", ".gitkeep":
+		return "git"
+	case ".go":
+		return "go"
+	case ".html", ".htm":
+		return "html"
+	case ".js":
+		return "javascript"
+	case ".json":
+		return "json"
+	case ".kt":
+		return "kotlin"
+	case ".lock":
+		return "yarn"
+	case ".md":
+		return "markdown"
+	case ".mp3", ".wav", ".flac", ".ogg":
+		return "audio"
+	case ".mp4", ".avi", ".mkv", ".mov":
+		return "video"
+	case ".png", "jpg", "jpeg", "bmp", "raw":
+		return "image"
+	case ".php":
+		return "php"
 	case ".py":
 		return "python"
+	case ".rb":
+		return "ruby"
+	case ".sass", "scss":
+		return "sass"
+	case ".sc":
+		return "scala"
+	case ".sh":
+		return "shell"
+	case ".ts":
+		return "typescript"
+	case ".tsx":
+		return "react"
 	default:
 		return "file"
+
 	}
 }
