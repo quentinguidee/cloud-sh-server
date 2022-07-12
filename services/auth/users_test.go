ca9d98c6f3ec723c8ea836e73fd3aea52de5947f
Quentin Guidée <quentin.guidee@gmail.com>
2022-07-12 20:45:32 +0200

    Add GetUser unit test
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

---
+++ services/auth/users_test.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"context"
+	"self-hosted-cloud/server/database"
+	"self-hosted-cloud/server/models"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+)
+
+func newDB() (database.Database, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		panic(err)
+	}
+	dbx := sqlx.NewDb(db, "sqlmock")
+	return database.New(dbx), mock
+}
+
+func newTX(db *database.Database) *sqlx.Tx {
+	tx, err := db.Instance.BeginTxx(context.Background(), nil)
+	if err != nil {
+		panic(err)
+	}
+	return tx
+}
+
+func TestGetUser(t *testing.T) {
+	db, mock := newDB()
+
+	rows := sqlmock.NewRows([]string{"id", "username", "name", "profile_picture"}).
+		AddRow(2, "username", "Name", "https://google.com/")
+
+	mock.ExpectBegin()
+	mock.ExpectQuery("^SELECT (.+) FROM users WHERE username = \\?$").
+		WithArgs("username").
+		WillReturnRows(rows)
+	mock.ExpectCommit()
+
+	tx := newTX(&db)
+
+	user, _ := GetUser(tx, "username")
+
+	assert.Equal(t, user, models.User{
+		Id:             2,
+		Username:       "username",
+		Name:           "Name",
+		ProfilePicture: "https://google.com/",
+	})
+}
