a783cbe6a6f577d1921b60074eb2895472597d99
Quentin Guidée <quentin.guidee@gmail.com>
2022-07-30 10:52:07 +0200

    Reset storage on /admin/reset
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/main/kotlin/com/quentinguidee/services/AdminsServices.kt
+++ src/main/kotlin/com/quentinguidee/services/AdminsServices.kt
@@ -2,11 +2,15 @@ package com.quentinguidee.services
 
 import com.quentinguidee.plugins.initDatabase
 import com.quentinguidee.plugins.resetDatabase
+import java.io.File
+import kotlin.io.path.Path
 
 class AdminsServices {
     suspend fun reset() {
         resetDatabase()
         initDatabase()
+
+        File(Path("data", "buckets").toString()).deleteRecursively()
     }
 }
 
