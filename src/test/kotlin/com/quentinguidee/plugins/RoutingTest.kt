a7cd24a34bbfa629fd57e04c188ec6ee43141e14
Quentin Guidée <quentin.guidee@gmail.com>
2022-07-31 18:10:55 +0200

    GitHub test workflow

--- src/test/kotlin/com/quentinguidee/plugins/RoutingTest.kt
+++ src/test/kotlin/com/quentinguidee/plugins/RoutingTest.kt
@@ -3,6 +3,7 @@ package com.quentinguidee.plugins
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.server.config.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -10,6 +11,10 @@ import kotlin.test.assertEquals
 class RoutingTest {
     @Test
     fun testGetPing() = testApplication {
+        environment {
+            config = ApplicationConfig("application.conf")
+        }
+
         application {
             configureRouting()
         }
