327253784229ae04fca6ee9f2996928dc53862a8
Quentin Guidée <quentin.guidee@gmail.com>
2022-08-08 19:43:20 +0200

    Remove unused test resources
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/test/kotlin/com/quentinguidee/plugins/RoutingTest.kt
+++ src/test/kotlin/com/quentinguidee/plugins/RoutingTest.kt
@@ -3,7 +3,6 @@ package com.quentinguidee.plugins
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.server.config.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -11,10 +10,6 @@ import kotlin.test.assertEquals
 class RoutingTest {
     @Test
     fun testGetPing() = testApplication {
-        environment {
-            config = ApplicationConfig("application.conf")
-        }
-
         application {
             configureRouting()
         }
